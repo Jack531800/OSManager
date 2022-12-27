5e79fe2d166ecca1e545fcb451fdd52379e3f840
Jack531800 <77768665+Jack531800@users.noreply.github.com>
2022-12-27 17:44:46 +0800

    Update CatalogManager.h

--- src/include/CatalogManager.h
+++ src/include/CatalogManager.h
@@ -6,31 +6,63 @@
 #define OSMANAGER_CATALOGMANAGER_H
 
 
-#include "DiskManager.h"
+#include<iostream>
+#include<string.h>
+#include<iomanip>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<cstdlib>
+#include<Windows.h>
+using namespace std;
+#define  _WINSOCK_DEPRECATED_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 
-class CatalogManager {
+typedef class CatalogManager {
 public:
     //构造函数根据自身情况添加参数
     CatalogManager(DiskManager *disk_manager);
-
+    
     ~CatalogManager() = default;
 
 
     //新建目录
-    void mkdir(std::string dir_name);
+    void mkdir(std::char dirName[]);
 
     //删除目录
-    void rmdir(std::string dir_name);
+    void rmdir(std::char dirName[]);
 
     //为文件建立目录项
-    void create_dentry(Inode inode);
+    void create_dentry(std::char fileName[]);
 
     //删除文件
-    void rmfile(std::string file_name);
+    void rmfile(std::char fileName[]);
 
+    bool IsInMaster(dirNode* p);
+    
+    void Write_File(dirNode* p);
+    void Read_File(dirNode* p);
+    void Transfer_In_File(dirNode* p);
+    void Transfer_Out_File(dirNode* p);
+    void Create_File(dirNode* p);
+    void Delete_File(dirNode* p);
 private:
     //用上的数据结构都放这
-};
+    char name[50];
+	int type;
+	struct tm t;
+	CatalogManager* next;
+	CatalogManager* sub;
+	CatalogManager* father;
+	int size;
+	char* str;
+	int addr;
+	WCHAR PCName[255];//直接使用wchar类型定义
+	WCHAR UserName[255];
+}dirNode;
+dirNode* workDir;
+dirNode root;
+char path[100];
 
 
 #endif //OSMANAGER_CATALOGMANAGER_H
